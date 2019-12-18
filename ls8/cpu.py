6595fb41ab80a22cd86a89d72f23a77e305b3aa3
Dustin <clerin+gitkraken@gmail.com>
2019-12-18 14:23:17 -0800

    finished call and ret. refactored code so pc increment happens in function instead of run to minimize errors in pc value that happen on functions that do not increment the pc.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,6 +8,7 @@ class CPU: # Main CPU class.
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.ir = 0
         self.running = False
         self.trans_instructions = {
             0b10000010: self.ldi,
@@ -19,8 +20,12 @@ class CPU: # Main CPU class.
             0b10100011: self.div,
             0b10100100: self.mod,
             0b01000101: self.push,
-            0b01000110: self.pop
+            0b01000110: self.pop,
+            0b01010000: self.call,
+            0b00010001: self.ret
         }
+        self.intm = 5 # Interrupt Mask
+        self.ints = 6 # Interrupt Status
         self.sp = 7 # Stack Pointer
         self.reg[self.sp] = 0xF4 # Decimal 243
 
@@ -55,7 +60,7 @@ class CPU: # Main CPU class.
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
+        # print("instructions",self.ram)
 
     def alu(self, op, reg_a, reg_b): # ALU operations. Arithmetic Logic Unit
         def add():
@@ -136,52 +141,76 @@ class CPU: # Main CPU class.
 
         print()
 
+    def call(self):
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = self.pc + ((self.ir & 0b11000000) >> 6) + 1
+        val = self.ram[self.pc+1]
+        self.pc = self.reg[val]
+    
+    def ret(self):
+        val = self.ram[self.reg[self.sp]]
+        self.pc = val
+        self.reg[self.sp] += 1
+
     def push(self):
         self.reg[self.sp] -= 1
         reg_num = self.ram[self.pc+1]
         reg_val = self.reg[reg_num]
         self.ram[self.reg[self.sp]] = reg_val
+        self.inc_pc()
     
     def pop(self):
         val = self.ram[self.reg[self.sp]]
         reg_num = self.ram[self.pc+1]
         self.reg[reg_num] = val
         self.reg[self.sp] += 1
+        self.inc_pc()
 
     def ldi(self):
         self.cpu_instructions("LDI", self.pc+1, self.pc+2)
+        self.inc_pc()
 
     def prn(self):
         self.cpu_instructions("PRN", self.pc+1)
+        self.inc_pc()
 
     def hlt(self):
         self.cpu_instructions("HLT")
+        self.inc_pc()
     
     def add(self):
         self.alu("ADD", self.pc+1, self.pc+2)
+        self.inc_pc()
     
     def sub(self):
         self.alu("SUB", self.pc+1, self.pc+2)
+        self.inc_pc()
     
     def mul(self):
         self.alu("MUL", self.pc+1, self.pc+2)
+        self.inc_pc()
     
     def div(self):
         self.alu("DIV", self.pc+1, self.pc+2)
+        self.inc_pc()
     
     def mod(self):
         self.alu("MOD", self.pc+1, self.pc+2)
+        self.inc_pc()
+
+    def inc_pc(self):
+        self.pc += ((self.ir & 0b11000000) >> 6) + 1
 
     def run(self):
         self.running = True
+        count = 0
 
         while self.running:
-            ir = self.ram_read(self.pc)
+            self.ir = self.ram_read(self.pc)
 
             try:
-                func = self.trans_instructions[ir]
+                func = self.trans_instructions[self.ir]
                 func()
-                self.pc += ((ir & 0b11000000) >> 6) + 1
             except:
                 raise Exception("Unsupported Instruction")
                 sys.exit(1)
